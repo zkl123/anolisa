6c4505f1e0f13b8107becef3d59385de1ba9709e
Kailong Zhou <zhoukailong.zkl@alibaba-inc.com>
2026-04-07 19:35:56 +0800

    feat(cosh): add /bug command：submit bug report

--- src/copilot-shell/packages/cli/src/ui/commands/bugCommand.ts
+++ src/copilot-shell/packages/cli/src/ui/commands/bugCommand.ts
@@ -32,7 +32,7 @@ export const bugCommand: SlashCommand = {
       .join('\n');
 
     let bugReportUrl =
-      'https://github.com/QwenLM/qwen-code/issues/new?template=bug_report.yml&title={title}&info={info}';
+      'https://github.com/alibaba/anolisa/issues/new?template=bug_report.yml&title={title}&environment={info}';
 
     const bugCommandSettings = context.services.config?.getBugCommand();
     if (bugCommandSettings?.urlTemplate) {
